34ff8c1c6a3a89e307b7b4eef756a8b2908e2dfa
manjugoutham <gouthamy219@gmail.com>
2021-12-20 16:27:25 +0530

    Added float program

--- Testmaximum.java
+++ Testmaximum.java
@@ -16,10 +16,22 @@ public class Testmaximum {
 			System.out.println(" Third is largest" + Z);
 	}
 
+	public void findfloat(Float X, Float Y, Float Z)
+	{
+		
+		if (X.compareTo(Y) >= 0 && X.compareTo(Z) >= 0) {
+			System.out.println(" First largest float value is: " + X);
+		} else if (Y.compareTo(X) >= 0 && Y.compareTo(Z) >= 0) {
+			System.out.println(" Second largest float value is:" + Y);
+		} else
+			System.out.println(" Third largest float value is" + Z);
+	}
+
 	
 	public static void main(String[] args) {
-
+		Testmaximum test = new Testmaximum();
 		test.findInteger(5, 10, 15);
+                test.findfloat(10.2f, 20.2f, 30.2f);
 
 	}
 
